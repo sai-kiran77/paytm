9db57da31f67cbb4a06f5beb9edffb067842e952
sai kiran <sai.kiran@nextiva.com>
2025-05-25 19:46:01 +0530

    user schema

---
+++ models/user.js
@@ -0,0 +1,45 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    balance: {
+      type: Number,
+      default: 0,
+    },
+
+    refreshTokens: [
+      {
+        token: String,
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.index({ email: 1 }); // helpful for login queries
+
+module.exports = mongoose.model("User", userSchema);
